4c9203ace8b5e38bb3ab4f9cc0eaa97631498961
Taylor L <tlandich@umich.edu>
2021-02-17 19:12:19 -0500

    updated text

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ import jdk.javadoc.internal.doclets.formats.html.resources.standard;
 public class Hello {
     public static void main(String[] args) {
         for(int i=1;i<=10;i++) {
-            System.out.printIn("Hello World..."+i);
+            System.out.printIn("Hello World...."+i);
         }
     }
 }
