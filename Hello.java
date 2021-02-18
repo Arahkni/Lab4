bf35f94e2e3a334d0a5259db5d36a88a11560f8f
Taylor L <tlandich@umich.edu>
2021-02-18 01:10:08 -0500

    fixed error in loop

--- Hello.java
+++ Hello.java
@@ -1,9 +1,9 @@
-import jdk.javadoc.internal.doclets.formats.html.resources.standard;
-
 public class Hello {
-    public static void main(String[] args) {
-        for(int i=1;i<=11;i++) {
-            System.out.printIn("Hello World..."+i);
-        }
-    }
+	public static void main(String[] args) {
+
+		for(int i=1;i<=10;i++) {
+
+			System.out.println("Hello World..."+i);
+		}
+	}
 }
